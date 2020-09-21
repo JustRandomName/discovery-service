63c28bac1daaa0781cc8282d9797344492016350
Nikita Zhuchkevich <n.zhuchkevich@itransition.com>
2020-09-21 16:24:10 +0300

    test jenkins

--- src/main/java/project/DiscoveryServiceApplication.java
+++ src/main/java/project/DiscoveryServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         run(DiscoveryServiceApplication.class, args);
     }
 
